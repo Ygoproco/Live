93844eec5cb8c06e0b70123248211247020007b9
EerieCode <eeriecode@gmail.com>
2015-11-21 17:25:52 +0100

    Added V-Jump cards, changed "D/D Savant"

---
+++ script/c6405.lua
@@ -0,0 +1,62 @@
+--Scripted by Eerie Code
+--Holding Arms
+function c6405.initial_effect(c)
+	--lock
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(6405,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(c6405.target)
+	e1:SetOperation(c6405.operation)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+
+function c6405.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function c6405.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not c:IsRelateToEffect(e) or not tc:IsRelateToEffect(e) or tc:IsFacedown() then return end
+	c:SetCardTarget(tc)
+	tc:RegisterFlagEffect(6405,RESET_EVENT+0x1fe0000,0,0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_OWNER_RELATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_DISABLE)
+	e1:SetReset(RESET_EVENT+0x1fe0000)
+	e1:SetCondition(c6405.rcon)
+	tc:RegisterEffect(e1,true)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	tc:RegisterEffect(e2,true)
+	local e3=Effect.CreateEffect(c)
+	e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
+	e3:SetTarget(c6405.indtg)
+	e3:SetValue(1)
+	e3:SetReset(RESET_EVENT+0x1fe0000)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	c:RegisterEffect(e4)
+end
+function c6405.rcon(e)
+	return e:GetOwner():IsHasCardTarget(e:GetHandler())
+end
+function c6405.indtg(e,c)
+	return e:GetHandler():IsHasCardTarget(c) and c:GetFlagEffect(76067258)~=0
+end
---
+++ script/c6920.lua
@@ -0,0 +1,69 @@
+--Scripted by Eerie Code
+--Sage Knight with Eyes of Blue
+function c6920.initial_effect(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(6920,0))
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(c6920.thtg)
+	e1:SetOperation(c6920.thop)
+	c:RegisterEffect(e1)
+	--Special Summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(6920,1))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_HAND)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCountLimit(1,6920)
+	e4:SetCost(c6920.spcost)
+	e4:SetTarget(c6920.sptg)
+	e4:SetOperation(c6920.spop)
+	c:RegisterEffect(e4)
+end
+
+function c6920.thfilter(c)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and not c:IsCode(6920) and c:IsType(TYPE_TUNER) and c:GetLevel()==1 and c:IsAbleToHand()
+end
+function c6920.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c6920.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c6920.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c6920.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+function c6920.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function c6920.spfilter(c,e,tp)
+	return c:IsSetCard(0xe0) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c6920.cfilter(c)
+	return c:IsType(TYPE_EFFECT) and c:IsAbleToGraveAsCost()
+end
+function c6920.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c6920.cfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c6920.cfilter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingMatchingCard(c6920.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,c6920.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function c6920.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SendtoGrave(tc,REASON_EFFECT+REASON_COST)>0 then
+		local g=Duel.SelectMatchingCard(tp,c6920.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if g:GetCount()>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
