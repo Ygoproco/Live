5943ced9eef23f6e53eeb18e708feb5f7a266ca9
EerieCode <eeriecode@gmail.com>
2015-12-24 09:58:27 +0100

    Added "Red Supremacy", "Magic King Moon Star" and "Freezing Spirit of the Monarchs", fixed some bugs.
    Fixed "Crystal Wing Synchro Dragon", "Ruins of the Great Divine Dragon"
    and "Harmony of the King's Soul"

---
+++ script/c50584941.lua
@@ -0,0 +1,45 @@
+--Scripted by Eerie Code
+--Red Supremacy
+function c50584941.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCost(c50584941.cost)
+	e1:SetTarget(c50584941.target)
+	e1:SetOperation(c50584941.activate)
+	c:RegisterEffect(e1)
+end
+
+function c50584941.cfilter(c)
+	return c:IsType(TYPE_SYNCHRO) and c:IsSetCard(0x1045) and c:IsAbleToRemoveAsCost()
+end
+function c50584941.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c50584941.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	local g=Duel.SelectMatchingCard(tp,c50584941.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+	e:SetLabel(g:GetFirst():GetCode())
+end
+function c50584941.filter(c)
+	return c:IsType(TYPE_SYNCHRO) and c:IsSetCard(0x1045) and c:IsFaceup()
+end
+function c50584941.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c50584941.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c50584941.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SelectTarget(tp,c50584941.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c50584941.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local code=e:GetLabel()
+	if tc:IsFaceup() and tc:IsRelateToEffect() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		e1:SetCode(EFFECT_CHANGE_CODE)
+		e1:SetValue(code)
+		tc:RegisterEffect(e1)
+		tc:CopyEffect(code,RESET_EVENT+0x1fe0000,1)
+	end
+end
---
+++ script/c99940363.lua
@@ -0,0 +1,58 @@
+--Scripted by Eerie Code
+--Freezing Spirit of the Monarchs
+function c99940363.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCondition(c99940363.condition)
+	e1:SetTarget(c99940363.target)
+	e1:SetOperation(c99940363.activate)
+	c:RegisterEffect(e1)
+	--Destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(c99940363.descost)
+	e2:SetTarget(c99940363.target)
+	e2:SetOperation(c99940363.activate)
+	c:RegisterEffect(e2)
+end
+
+function c99940363.cfilter(c)
+	return c:IsFaceup() and c:IsAttackAbove(2400) and c:GetDefence()==1000
+end
+function c99940363.filter(c)
+	return c:IsFacedown() and c:IsDestructable()
+end
+function c99940363.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c99940363.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c99940363.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and c99940363.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c99940363.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c99940363.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c99940363.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFacedown() and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+
+function c99940363.cfilter2(c)
+	return c:IsSetCard(0xbe) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToRemoveAsCost()
+end
+function c99940363.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost()
+		and Duel.IsExistingMatchingCard(c99940363.cfilter2,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c99940363.cfilter2,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+	g:AddCard(e:GetHandler())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
