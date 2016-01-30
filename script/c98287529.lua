0d3bd83308c9abbc2c8bd7e794e53817c34f67d1
Joshua Ennew <joshua.ennew@students.plymouth.ac.uk>
2016-01-30 11:50:09 +0000

    Amorphactor Psycho, the Vain Dracoverlord
    Fixed summoning issue.

--- script/c98287529.lua
+++ script/c98287529.lua
@@ -2,13 +2,6 @@
 --Amorphactor Psycho, the Vain Dracoverlord
 function c98287529.initial_effect(c)
 	c:EnableReviveLimit()
-	--spsummon condition
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(aux.ritlimit)
-	c:RegisterEffect(e1)
 	--Skip Main Phase
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
