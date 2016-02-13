978cf3f8a6865568b60ffc710ac1846560e1c562
EerieCode <eeriecode@gmail.com>
2016-02-13 12:13:45 +0100

    Added DF16 cards

--- script/c47435107.lua
+++ script/c47435107.lua
@@ -1,7 +1,7 @@
 --Scripted by Eerie Code
 --Primal Cry
-function c6123.initial_effect(c)
-	aux.AddRitualProcGreaterCode(c,6122)
+function c47435107.initial_effect(c)
+	aux.AddRitualProcGreaterCode(c,10441498)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
@@ -9,41 +9,41 @@ function c6123.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_GRAVE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(c6123.spcon)
-	e1:SetCost(c6123.spcost)
-	e1:SetTarget(c6123.sptg)
-	e1:SetOperation(c6123.spop)
+	e1:SetCondition(c47435107.spcon)
+	e1:SetCost(c47435107.spcost)
+	e1:SetTarget(c47435107.sptg)
+	e1:SetOperation(c47435107.spop)
 	c:RegisterEffect(e1)
 end
 
-function c6123.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c47435107.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
-function c6123.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c47435107.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
-function c6123.spfil(c,e,tp,turn)
+function c47435107.spfil(c,e,tp,turn)
 	return c:IsType(TYPE_RITUAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsPreviousLocation(LOCATION_ONFIELD) and c:GetTurnID()==turn
 	--if not c:IsType(TYPE_RITUAL) then
-	--	return false
+	--  return false
 	--elseif not c:IsPreviousLocation(LOCATION_ONFIELD) then
-	--	Debug.Message(""..c:GetCode().." was not on field.")
-	--	return false
+	--  Debug.Message(""..c:GetCode().." was not on field.")
+	--  return false
 	--elseif c:GetTurnID()~=turn then
 	 --   Debug.Message(""..c:GetCode().." wasn't sent this turn.")
-	--	return false
+	--  return false
 	--else return c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
 end
-function c6123.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c47435107.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local turn=Duel.GetTurnCount()
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c6123.spfil(chkc,e,tp,turn) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(c6123.spfil,tp,LOCATION_GRAVE,0,1,nil,e,tp,turn) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c47435107.spfil(chkc,e,tp,turn) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(c47435107.spfil,tp,LOCATION_GRAVE,0,1,nil,e,tp,turn) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c6123.spfil,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,turn)
+	local g=Duel.SelectTarget(tp,c47435107.spfil,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,turn)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,g:GetCount(),0,0)
 end
-function c6123.spop(e,tp,eg,ep,ev,re,r,rp)
+function c47435107.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
