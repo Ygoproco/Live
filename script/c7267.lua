c2549bb48a12c60087e39494888554bff0c4ce59
EerieCode <eeriecode@gmail.com>
2016-03-16 07:38:55 +0100

    Fixed "Red Nova"

--- script/c7267.lua
+++ script/c7267.lua
@@ -29,7 +29,7 @@ function c7267.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c77360173.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+		and Duel.IsExistingMatchingCard(c7267.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end
 
 function c7267.syncon(e,tp,eg,ep,ev,re,r,rp)
