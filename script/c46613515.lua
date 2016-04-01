3303bd5a80e49c7a5a2fbc84a554e339181e53bb
EerieCode <eeriecode@gmail.com>
2016-04-01 21:43:11 +0200

    Official code for "Clear Kuriboh"

--- script/c46613515.lua
+++ script/c46613515.lua
@@ -1,34 +1,34 @@
 --Scripted by Eerie Code
 --Clear Kuriboh
-function c6121.initial_effect(c)
+function c46613515.initial_effect(c)
 	--Negate effect
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(6121,0))
+	e1:SetDescription(aux.Stringid(46613515,0))
 	e1:SetCategory(CATEGORY_NEGATE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c6121.negcon)
-	e1:SetCost(c6121.negcost)
-	e1:SetTarget(c6121.negtg)
-	e1:SetOperation(c6121.negop)
+	e1:SetCondition(c46613515.negcon)
+	e1:SetCost(c46613515.negcost)
+	e1:SetTarget(c46613515.negtg)
+	e1:SetOperation(c46613515.negop)
 	c:RegisterEffect(e1)
 	--Draw
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_DRAW+CATEGORY_SPECIAL_SUMMON)
-	e2:SetDescription(aux.Stringid(6121,1))
+	e2:SetDescription(aux.Stringid(46613515,1))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,6121)
-	e2:SetCondition(c6121.atkcon)
-	e2:SetCost(c6121.atkcost)
-	e2:SetTarget(c6121.atktg)
-	e2:SetOperation(c6121.atkop)
+	e2:SetCountLimit(1,46613515)
+	e2:SetCondition(c46613515.atkcon)
+	e2:SetCost(c46613515.atkcost)
+	e2:SetTarget(c46613515.atktg)
+	e2:SetOperation(c46613515.atkop)
 	c:RegisterEffect(e2)
 end
 
-function c6121.negcon(e,tp,eg,ep,ev,re,r,rp)
+function c46613515.negcon(e,tp,eg,ep,ev,re,r,rp)
 	if rp==tp or not re:IsActiveType(TYPE_MONSTER) or not Duel.IsChainNegatable(ev) then return false end
 	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
 	if ex then return true end
@@ -39,40 +39,40 @@ function c6121.negcon(e,tp,eg,ep,ev,re,r,rp)
 		or Duel.IsPlayerAffectedByEffect(1,EFFECT_REVERSE_RECOVER)
 	end
 end
-function c6121.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c46613515.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsDiscardable() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
 	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 end
-function c6121.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c46613515.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 end
-function c6121.negop(e,tp,eg,ep,ev,re,r,rp)
+function c46613515.negop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateActivation(ev)
 end
 
-function c6121.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function c46613515.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetAttacker():IsControler(1-tp) and Duel.GetAttackTarget()==nil
 end
-function c6121.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c46613515.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
-function c6121.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c46613515.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function c6121.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c46613515.atkop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.Draw(tp,1,REASON_EFFECT)==0 then return end
 	local tc=Duel.GetOperatedGroup():GetFirst()
 	Duel.ConfirmCards(1-tp,tc)
 	if tc:IsType(TYPE_MONSTER) then
 		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-		if tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.SelectYesNo(tp,aux.Stringid(6121,2)) then
+		if tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.SelectYesNo(tp,aux.Stringid(46613515,2)) then
 			if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) then
 				local a=Duel.GetAttacker()
-				if a:IsAttackable() and not a:IsImmuneToEffect(e) then			
+				if a:IsAttackable() and not a:IsImmuneToEffect(e) then		  
 					Duel.CalculateDamage(a,tc)
 				end
 			end
