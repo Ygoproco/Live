95c628d019cbf568977249c5aaeb2d3907ae146c
EerieCode <eeriecode@gmail.com>
2016-04-16 09:20:19 +0200

    Add "Blue-Eyes Chaos MAX Dragon"

---
+++ script/c7204.lua
@@ -0,0 +1,48 @@
+--Scripted by Eerie Code
+--Blue-Eyes Chaos MAX Dragon
+function c7204.initial_effect(c)
+	c:EnableReviveLimit()
+	--spsummon condition
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(aux.ritlimit)
+	c:RegisterEffect(e0)
+	--Immune
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(aux.tgoval)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetValue(c7204.tgvalue)
+	c:RegisterEffect(e2)
+	--pierce
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetCode(EFFECT_PIERCE)
+	c:RegisterEffect(e6)
+	--double
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e4:SetCondition(c7204.damcon)
+	e4:SetOperation(c7204.damop)
+	c:RegisterEffect(e4)
+	
+end
+
+function c7204.tgvalue(e,re,rp)
+	return rp~=e:GetHandlerPlayer()
+end
+
+function c7204.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and e:GetHandler():GetBattleTarget()~=nil and e:GetHandler():GetBattleTarget():IsDefencePos()
+end
+function c7204.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeBattleDamage(ep,ev*2)
+end
