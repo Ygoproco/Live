5ff17dc8726c7edc4428d25477f46ea125377a53
EerieCode <eeriecode@gmail.com>
2016-04-19 12:14:06 +0200

    Fixed "Cyber Angel - Dakini"

--- script/c7264.lua
+++ script/c7264.lua
@@ -60,7 +60,7 @@ function c7264.thfil(c)
 end
 function c7264.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
   if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c7264.thfil(chkc) end
-  if chk==0 then return Duel.IsExistingTarget(c7264.thfil.tp,LOCATION_GRAVE,0,1,nil) end
+  if chk==0 then return Duel.IsExistingTarget(c7264.thfil,tp,LOCATION_GRAVE,0,1,nil) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
   local g=Duel.SelectTarget(tp,c7264.thfil,tp,LOCATION_GRAVE,0,1,1,nil)
   Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
