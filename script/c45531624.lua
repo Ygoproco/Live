773b9cfd6abaf78815ae94612ac60be81a8f1ada
EerieCode <eeriecode@gmail.com>
2016-04-21 20:23:29 +0200

    Official picture for "Celtic Guard of Noble Arms"

---
+++ script/c45531624.lua
@@ -0,0 +1,69 @@
+﻿--Scripted by Eerie Code
+--Celtic Sacred Guard
+function c45531624.initial_effect(c)
+	--cannot attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	e1:SetCondition(c45531624.atcon)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(45531624,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c45531624.sptg)
+	e2:SetOperation(c45531624.spop)
+	c:RegisterEffect(e2)
+	--remove
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetDescription(aux.Stringid(45531624,1))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_DAMAGE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCondition(c45531624.drcon)
+	e3:SetTarget(c45531624.drtg)
+	e3:SetOperation(c45531624.drop)
+	c:RegisterEffect(e3)
+end
+
+function c45531624.atcon(e)
+	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_HAND,0)>=1
+end
+
+function c45531624.spfil(c,e,tp)
+	return c:IsSetCard(0xe4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c45531624.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c45531624.spfil,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function c45531624.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c45531624.spfil,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+function c45531624.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and Duel.GetAttacker()==e:GetHandler()
+end
+function c45531624.drfil(c)
+	return c:IsFaceup() and c:IsSetCard(0xe4)
+end
+function c45531624.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	local ct=Duel.GetMatchingGroupCount(c45531624.drfil,tp,LOCATION_MZONE,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,ct)
+end
+function c45531624.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local d=Duel.GetMatchingGroupCount(c45531624.drfil,tp,LOCATION_MZONE,0,nil)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
