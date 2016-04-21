f3c839e297fc22e7f4d9079de662b7f03aea671f
YgoPro-Sahim <loris.cangini@hotmail.it>
2016-04-21 16:54:20 +0200

    Added "ABC-Dragon Buster" (#261)
    Added "ABC-Dragon Buster"

---
+++ script/c7024.lua
@@ -0,0 +1,136 @@
+--ABC-Dragon Buster
+function c7024.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcCode3(c,7021,7022,7023,true,true)
+	--spsummon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(c7024.splimit)
+	c:RegisterEffect(e1)
+	--special summon rule
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_EXTRA)
+	e2:SetCondition(c7024.spcon)
+	e2:SetOperation(c7024.spop)
+	c:RegisterEffect(e2)
+	
+	--banish
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(7024,0))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCost(c7024.descost)
+	e3:SetTarget(c7024.destg)
+	e3:SetOperation(c7024.desop)
+	c:RegisterEffect(e3)
+	
+	--summon 3 banished monsters
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(7024,1))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(c7024.rmcon)
+	e4:SetCost(c7024.rmcost)
+	e4:SetTarget(c7024.rmtg)
+	e4:SetOperation(c7024.rmop)
+	c:RegisterEffect(e4)
+end
+function c7024.splimit(e,se,sp,st)
+	return not e:GetHandler():IsLocation(LOCATION_EXTRA)
+end
+
+function c7024.spfilter(c,code)
+	return c:IsCode(code) and c:IsAbleToRemoveAsCost()
+end
+function c7024.spcon(e,c)
+	if c==nil then return true end 
+	local tp=c:GetControler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<-2 then return false end
+	local g1=Duel.GetMatchingGroup(c7024.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil,7021)
+	local g2=Duel.GetMatchingGroup(c7024.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil,7022)
+	local g3=Duel.GetMatchingGroup(c7024.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil,7023)
+	if g1:GetCount()==0 or g2:GetCount()==0 or g3:GetCount()==0 then return false end
+	if ft>0 then return true end
+	local f1=g1:FilterCount(Card.IsLocation,nil,LOCATION_MZONE)>0 and 1 or 0
+	local f2=g2:FilterCount(Card.IsLocation,nil,LOCATION_MZONE)>0 and 1 or 0
+	local f3=g3:FilterCount(Card.IsLocation,nil,LOCATION_MZONE)>0 and 1 or 0
+	if ft==-2 then return f1+f2+f3==3
+	elseif ft==-1 then return f1+f2+f3>=2
+	else return f1+f2+f3>=1 end
+end
+function c7024.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local g1=Duel.GetMatchingGroup(c7024.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil,7021)
+	local g2=Duel.GetMatchingGroup(c7024.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil,7022)
+	local g3=Duel.GetMatchingGroup(c7024.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil,7023)
+	g1:Merge(g2)
+	g1:Merge(g3)
+	local g=Group.CreateGroup()
+	local tc=nil
+	for i=1,3 do
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		if ft<=0 then
+			tc=g1:FilterSelect(tp,Card.IsLocation,1,1,nil,LOCATION_MZONE):GetFirst()
+		else
+			tc=g1:Select(tp,1,1,nil):GetFirst()
+		end
+		g:AddCard(tc)
+		g1:Remove(Card.IsCode,nil,tc:GetCode())
+		ft=ft+1
+	end
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+
+function c7024.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function c7024.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,g:GetCount(),0,0)
+end
+function c7024.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
+
+function c7024.rmfilter(c,e,tp)
+	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsType(TYPE_UNION)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c7024.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function c7024.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c7024.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and c7024.rmfilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c7024.rmfilter,tp,LOCATION_REMOVED,0,3,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c7024.rmfilter,tp,LOCATION_REMOVED,0,3,3,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c7024.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local ex1,g=Duel.GetOperationInfo(0,CATEGORY_SPECIAL_SUMMON)
+	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+end
