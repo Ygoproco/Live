2c43e450e651d75e12f0ded1124f90d49e8c7dd4
EerieCode <eeriecode@gmail.com>
2016-04-23 18:23:20 +0200

    Added new cards
    "Final Geases"
    "Dark Horizon"
    "Gate of the Magical Contract"
    "Dark Magic Veil"

---
+++ script/c16964437.lua
@@ -0,0 +1,34 @@
+--ダーク・ホライズン
+--Dark Horizon
+--Scripted by Eerie Code
+function c16964437.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_DAMAGE)
+	e1:SetCondition(c16964437.condition)
+	e1:SetTarget(c16964437.target)
+	e1:SetOperation(c16964437.activate)
+	c:RegisterEffect(e1)
+end
+
+function c16964437.condition(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp
+end
+function c16964437.filter(c,e,tp,dam)
+	return c:IsAttackBelow(dam) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c16964437.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c16964437.filter,tp,LOCATION_DECK,0,1,nil,e,tp,ev) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function c16964437.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c16964437.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp,ev)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
---
+++ script/c18809562.lua
@@ -0,0 +1,34 @@
+--魔導契約の扉
+--Gate of the Magical Contract
+--Scripted by Eerie Code
+function c18809562.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c18809562.target)
+	e1:SetOperation(c18809562.activate)
+	c:RegisterEffect(e1)
+end
+
+function c18809562.fil(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and (c:GetLevel()==7 or c:GetLevel()==8) and c:IsAbleToHand()
+end
+function c18809562.target(e,tp,eg,ep,ev,re,r,rp)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_HAND,0,1,nil,TYPE_SPELL) and Duel.IsExistingMatchingCard(c18809562.fil,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
+end
+function c18809562.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_HAND,0,1,1,nil,TYPE_SPELL)
+	if g:GetCount()==0 then return end
+	Duel.SendtoHand(g,1-tp,REASON_EFFECT)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g2=Duel.SelectMatchingCard(tp,c18809562.fil,tp,LOCATION_DECK,0,1,1,nil)
+	if g2:GetCount()>0 then
+		Duel.BreakEffect()
+		Duel.SendtoHand(g2,tp,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g2)
+	end
+end
