a70908649d3523a7a1b09e46763660793e843d88
EerieCode <eeriecode@gmail.com>
2016-04-29 21:17:30 +0200

    Fixed "Galaxy-Eyes Cipher Dragon"

--- script/c7280.lua
+++ script/c7280.lua
@@ -46,30 +46,35 @@ function c7280.operation(e,tp,eg,ep,ev,re,r,rp)
   local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
+		local e4=Effect.CreateEffect(c)
+		  e4:SetType(EFFECT_TYPE_SINGLE)
+		  e4:SetCode(EFFECT_CHANGE_CODE)
+		  e4:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		  e4:SetValue(7280)
+		tc:RegisterEffect(e4)
 	  if Duel.GetControl(tc,tp,PHASE_END,1) then
 		local e1=Effect.CreateEffect(c)
 		  e1:SetType(EFFECT_TYPE_SINGLE)
 		  e1:SetCode(EFFECT_DISABLE)
-		  e1:SetReset(RESET_EVENT+0x1fe0000)
+		  e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
 		  tc:RegisterEffect(e1)
 		  local e2=e1:Clone()
 		  e2:SetCode(EFFECT_DISABLE_EFFECT)
-		  e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
 		  tc:RegisterEffect(e2)
 		  local e3=e2:Clone()
 		  e3:SetCode(EFFECT_SET_ATTACK_FINAL)
 		  e3:SetValue(3000)
 		  tc:RegisterEffect(e3)
-		  local e4=e2:Clone()
-		  e4:SetCode(EFFECT_CHANGE_CODE)
-		  e4:SetValue(7280)
-		  tc:RegisterEffect(e4)
 	else
+		tc:RegisterFlagEffect(7280,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
 	  if not tc:IsImmuneToEffect(e) and tc:IsAbleToChangeControler() then
 			  Duel.Destroy(tc,REASON_EFFECT)
 		  end
 	end
   end
+end
+function c7280.nccon(e)
+	return e:GetHandler():GetFlagEffect(7280)==0
 end
 function c7280.ftarget(e,c)
 	return e:GetLabel()~=c:GetFieldID()
