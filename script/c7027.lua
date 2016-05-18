b89672ad38f03b945b7ae27c697924048101f6eb
Sahim <loris.cangini@hotmail.it>
2016-05-18 13:28:43 +0200

    Fixed "Union Hangar"

--- script/c7027.lua
+++ script/c7027.lua
@@ -1,20 +1,11 @@
 --Union Hangar
 function c7027.initial_effect(c)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(7027,0))
-	e1:SetCountLimit(1,7027+EFFECT_COUNT_CODE_OATH)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
-	--Activate
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(7027,1))
 	e2:SetCountLimit(1,7027+EFFECT_COUNT_CODE_OATH)
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_ACTIVATE)
 	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetTarget(c7027.target)
 	e2:SetOperation(c7027.activate)
 	c:RegisterEffect(e2)
 	--equip from deck
@@ -35,14 +26,10 @@ end
 function c7027.filter(c)
 	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_MACHINE) and c:IsType(TYPE_UNION) and c:IsAbleToHand()
 end
-function c7027.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c7027.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-end
 function c7027.activate(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c7027.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
+	if Duel.IsExistingMatchingCard(c7027.filter,tp,LOCATION_DECK,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(7027,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,c7027.filter,tp,LOCATION_DECK,0,1,1,nil)
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
