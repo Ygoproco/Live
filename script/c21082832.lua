57b7016466fa6b5f85de55cb7e3864efa19f0533
Steeldarkeagel <steeldarkeagel@gmail.com>
2016-06-01 09:50:06 +0100

    Chaos Form
    Fixed bugs.

--- script/c21082832.lua
+++ script/c21082832.lua
@@ -12,25 +12,14 @@ function c21082832.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 c21082832.dark_magician_list=true
-
-function c21082832.filter(c,e,tp,m1,m2,ft)
+function c21082832.filter(c,e,tp,m1,m2)
 	if not c:IsSetCard(0xcf) or bit.band(c:GetType(),0x81)~=0x81 or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
 	local mg=m1:Filter(Card.IsCanBeRitualMaterial,c,c)
 	mg:Merge(m2)
 	if c.mat_filter then
 		mg=mg:Filter(c.mat_filter,nil)
 	end
-	if ft>0 then
-		return mg:CheckWithSumEqual(Card.GetRitualLevel,c:GetLevel(),1,99,c)
-	else
-		return ft>-1 and mg:IsExists(c21082832.mfilterf,1,nil,tp,mg,c)
-	end
-end
-function c21082832.mfilterf(c,tp,mg,rc)
-	if c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) then
-		Duel.SetSelectedCard(c)
-		return mg:CheckWithSumEqual(Card.GetRitualLevel,rc:GetLevel(),0,99,rc)
-	else return false end
+	return mg:CheckWithSumEqual(Card.GetRitualLevel,c:GetLevel(),1,99,c)
 end
 function c21082832.mfilter(c)
 	return (c:IsCode(89631139) or c:IsCode(46986414)) and c:IsAbleToRemove()
@@ -39,44 +28,26 @@ function c21082832.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local mg1=Duel.GetRitualMaterial(tp)
 		local mg2=Duel.GetMatchingGroup(c21082832.mfilter,tp,LOCATION_GRAVE,0,nil)
-		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		return Duel.IsExistingMatchingCard(c21082832.filter,tp,LOCATION_HAND,0,1,nil,e,tp,mg1,mg2,ft)
+		return Duel.IsExistingMatchingCard(c21082832.filter,tp,LOCATION_HAND,0,1,nil,e,tp,mg1,mg2)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function c21082832.activate(e,tp,eg,ep,ev,re,r,rp)
 	local mg1=Duel.GetRitualMaterial(tp)
 	local mg2=Duel.GetMatchingGroup(c21082832.mfilter,tp,LOCATION_GRAVE,0,nil)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=Duel.SelectMatchingCard(tp,c21082832.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,mg1,mg2,ft)
+	local tg=Duel.SelectMatchingCard(tp,c21082832.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,mg1,mg2)
 	local tc=tg:GetFirst()
 	if tc then
 		local mg=mg1:Filter(Card.IsCanBeRitualMaterial,tc,tc)
 		mg:Merge(mg2)
-		if tc:IsCode(21105106) then
-			tc:ritual_custom_operation(mg)
-			local mat=tc:GetMaterial()
-			Duel.ReleaseRitualMaterial(mat)
-		else
-			if tc.mat_filter then
-				mg=mg:Filter(tc.mat_filter,nil)
-			end
-			local mat=nil
-			if ft>0 then
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-				mat=mg:SelectWithSumEqual(tp,Card.GetRitualLevel,tc:GetLevel(),1,99,tc)
-			else
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-				mat=mg:FilterSelect(tp,c21082832.mfilterf,1,1,nil,tp,mg,tc)
-				Duel.SetSelectedCard(mat)
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-				local mat2=mg:SelectWithSumEqual(tp,Card.GetRitualLevel,tc:GetLevel(),0,99,tc)
-				mat:Merge(mat2)
-			end
-			tc:SetMaterial(mat)
-			Duel.ReleaseRitualMaterial(mat)
+		if tc.mat_filter then
+			mg=mg:Filter(tc.mat_filter,nil)
 		end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local mat=mg:SelectWithSumEqual(tp,Card.GetRitualLevel,tc:GetLevel(),1,99,tc)
+		tc:SetMaterial(mat)
+		Duel.ReleaseRitualMaterial(mat)
 		Duel.BreakEffect()
 		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
 		tc:CompleteProcedure()
