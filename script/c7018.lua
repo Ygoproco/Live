5c5313ab07aa03a98c5c864a5cc4e58a50783ca8
EerieCode <eeriecode@gmail.com>
2016-06-03 11:35:02 +0200

    Fixed "Dark Magic Succession"

--- script/c7018.lua
+++ script/c7018.lua
@@ -26,7 +26,7 @@ function c7018.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function c7018.fil(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP) and (c.dark_magician_list or c.dark_magician_girl_list) and c:IsAbleToHand()
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and (c.dark_magician_list or c.dark_magician_girl_list) and not c:IsCode(7018) and c:IsAbleToHand()
 end
 function c7018.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c7018.fil,tp,LOCATION_DECK,0,1,nil) end
