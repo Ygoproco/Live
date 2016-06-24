5f77bb327adb12b0c7958c8fe3862996a22f8586
kenpazaraki <kenpachi89@gmail.com>
2016-06-24 23:57:53 +0200

    Fixed "Crystron Quon"

--- script/c7315.lua
+++ script/c7315.lua
@@ -26,25 +26,29 @@ function c7315.spcon(e,tp,eg,ep,ev,re,r,rp)
 	if PHASE_BATTLE_START then bpc2=(ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE) end
 	return Duel.GetTurnPlayer()~=tp and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2 or bpc1 or bpc2)
 end
-function c7315.spfil(c,e,tp,mc)
-		local mg=Group.FromCards(c,mc)
-		return not c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-				and Duel.IsExistingMatchingCard(c7315.synfil,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg)
+function c7315.filter(c,e,tp,mc)
+	local mg=Group.FromCards(c,mc)
+	return not c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(c7315.scfilter,tp,LOCATION_EXTRA,0,1,nil,mg)
 end
-function c7315.synfil(c,mg)
-		return c:IsRace(RACE_MACHINE) and c:IsSynchroSummonable(nil,mg)
+function c7315.scfilter(c,mg)
+	return c:IsRace(RACE_MACHINE) and c:IsSynchroSummonable(nil,mg)
 end
-function c7315.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-		if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c7315.spfil,tp,LOCATION_HAND,0,1,nil,e,tp,e:GetHandler()) end
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+function c7315.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_HAND) and chkc:IsControler(tp) and c7315.filter(chkc,e,tp,e:GetHandler()) end
+	if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c7315.filter,tp,LOCATION_HAND,0,1,nil,e,tp,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c7315.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c7315.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
 	local c=e:GetHandler()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=Duel.SelectMatchingCard(tp,c7315.spfil,tp,LOCATION_HAND,0,1,1,nil,e,tp,c)
-	local tc=tg:GetFirst()
-	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+	local tc=Duel.GetFirstTarget()
+	if not c:IsRelateToEffect(e) then return end
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
 			local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
@@ -56,8 +60,8 @@ function c7315.spop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetReset(RESET_EVENT+0x1fe0000)
 		tc:RegisterEffect(e2,true)
 		Duel.SpecialSummonComplete()
-		local mg=Group.FromCards(c,tc)
-			local g=Duel.GetMatchingGroup(c7315.synfil,tp,LOCATION_EXTRA,0,nil,mg)
+			local mg=Group.FromCards(c,tc)
+			local g=Duel.GetMatchingGroup(c7315.scfilter,tp,LOCATION_EXTRA,0,nil,mg)
 			if g:GetCount()>0 then
 				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 				local sg=g:Select(tp,1,1,nil)
