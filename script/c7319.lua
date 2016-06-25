df4f7c5763466e8c2de6866d810a499cf3a338e4
EerieCode <eeriecode@gmail.com>
2016-06-25 18:08:18 +0200

    Bug fixes for "Crystron" monsters

--- script/c7319.lua
+++ script/c7319.lua
@@ -68,7 +68,7 @@ function c7319.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c7319.splimit(e,c,tp,sumtp,sumpos)
-	return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_SYNCHRO) and not c:IsRace(RACE_MACHINE)
+	return not (c:IsRace(RACE_MACHINE) and c:IsType(TYPE_SYNCHRO)) and c:IsLocation(LOCATION_EXTRA)
 end
 
 function c7319.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
