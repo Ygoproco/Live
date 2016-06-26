66fed3bf0354d31681a0c1449b673969c267143a
Steeldarkeagel <steeldarkeagel@gmail.com>
2016-06-26 12:49:12 +0100

    Utility fix

--- script/utility.lua
+++ script/utility.lua
@@ -1385,6 +1385,7 @@ function Auxiliary.PendOperation()
 				local rscale=rpz:GetRightScale()
 				if lscale>rscale then lscale,rscale=rscale,lscale end
 				local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+				if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=math.min(ft,1) end
 				if og then
 					Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 					local g=og:FilterSelect(tp,Auxiliary.PConditionFilter,1,ft,nil,e,tp,lscale,rscale)
