f2f8a67458ae0c51ac5a44f5f9103fb1d32425b3
EerieCode <eeriecode@gmail.com>
2016-06-29 20:37:04 +0200

    Official number for "Divine Serpent Geh"

--- script/c82103466.lua
+++ script/c82103466.lua
@@ -1,6 +1,6 @@
 --Divine Serpent Geh
 --Scripted by Sahim & Kenpazaraki
-function c6132.initial_effect(c)
+function c82103466.initial_effect(c)
 	c:EnableReviveLimit()
 	--connot special summon
 	local e0=Effect.CreateEffect(c)
@@ -15,10 +15,10 @@ function c6132.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CHAIN_UNIQUE)
 	e1:SetCode(EVENT_DESTROYED)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c6132.spcon)
-	e1:SetCost(c6132.spcost)
-	e1:SetTarget(c6132.sptg)
-	e1:SetOperation(c6132.spop)
+	e1:SetCondition(c82103466.spcon)
+	e1:SetCost(c82103466.spcost)
+	e1:SetTarget(c82103466.sptg)
+	e1:SetOperation(c82103466.spop)
 	c:RegisterEffect(e1)
 	--cannot be target
 	local e2=Effect.CreateEffect(c)
@@ -34,8 +34,8 @@ function c6132.initial_effect(c)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,LOCATION_MZONE)
 	e3:SetCode(EFFECT_DISABLE)
-	e3:SetCondition(c6132.discon)
-	e3:SetTarget(c6132.distg)
+	e3:SetCondition(c82103466.discon)
+	e3:SetTarget(c82103466.distg)
 	c:RegisterEffect(e3)
 	local e4=e3:Clone()
 	e4:SetCode(EFFECT_DISABLE_EFFECT)
@@ -43,7 +43,7 @@ function c6132.initial_effect(c)
 	--atk change
 	local e5=e3:Clone()
 	e5:SetCode(EFFECT_SET_ATTACK_FINAL)
-	e5:SetValue(c6132.atkval)
+	e5:SetValue(c82103466.atkval)
 	c:RegisterEffect(e5)
 	--gain ATK
 	local e6=Effect.CreateEffect(c)
@@ -52,52 +52,52 @@ function c6132.initial_effect(c)
 	e6:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e6:SetRange(LOCATION_MZONE)
 	e6:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e6:SetCost(c6132.atkcost)
-	e6:SetOperation(c6132.atkop)
+	e6:SetCost(c82103466.atkcost)
+	e6:SetOperation(c82103466.atkop)
 	c:RegisterEffect(e6)
 end
-function c6132.cfilter(c,tp,rp)
+function c82103466.cfilter(c,tp,rp)
 	return (c:IsReason(REASON_BATTLE) and c:GetPreviousControler()==tp and c==Duel.GetAttackTarget())
 		or (rp~=tp and c:IsReason(REASON_DESTROY) and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE))
 end
-function c6132.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c82103466.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return eg:IsExists(c6132.cfilter,1,nil,tp,rp)
+	return eg:IsExists(c82103466.cfilter,1,nil,tp,rp)
 end
-function c6132.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c82103466.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
 end
-function c6132.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c82103466.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,true) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c6132.spop(e,tp,eg,ep,ev,re,r,rp)
+function c82103466.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsLocation(LOCATION_HAND) and Duel.SpecialSummon(c,0,tp,tp,true,true,POS_FACEUP)~=0 then
 		c:CompleteProcedure()
 	end
 end
 
-function c6132.discon(e)
+function c82103466.discon(e)
 	local ph=Duel.GetCurrentPhase()
 	return (ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL)
 		and Duel.GetAttacker()==e:GetHandler() and Duel.GetAttackTarget()~=nil
 end
-function c6132.distg(e,c)
+function c82103466.distg(e,c)
 	return c==e:GetHandler():GetBattleTarget()
 end
-function c6132.atkval(e,c)
+function c82103466.atkval(e,c)
 	return math.ceil(e:GetHandler():GetBattleTarget():GetBaseAttack()/2)
 end
 
-function c6132.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c82103466.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:GetFlagEffect(6132)==0 and (Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler()) end
-	c:RegisterFlagEffect(6132,RESET_CHAIN,0,1)
+	if chk==0 then return c:GetFlagEffect(82103466)==0 and (Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler()) end
+	c:RegisterFlagEffect(82103466,RESET_CHAIN,0,1)
 end
-function c6132.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c82103466.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
 		local tg,val = Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,c):GetMaxGroup(Card.GetBaseAttack)
