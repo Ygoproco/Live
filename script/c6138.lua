ae67c6c0a04eefab1799971bb4a3c005ef85659a
EerieCode <eeriecode@gmail.com>
2016-07-07 21:51:00 +0200

    Added new cards
    "Abyss Actor - Evil Heel"
    "Abyss Actor - Funky Comedian"
    "Abyss Actor - Dandy Supporting Actor"

---
+++ script/c6138.lua
@@ -0,0 +1,80 @@
+--魔界劇団－ダンディ・バイプレイヤー
+--Abyss Actor - Dandy Supporting Actor
+--Scripted by Eerie Code
+function c6138.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(6138,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCondition(c6138.thcon)
+	e1:SetTarget(c6138.thtg)
+	e1:SetOperation(c6138.thop)
+	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(6138,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,6138)
+	e2:SetCondition(c6138.spcon)
+	e2:SetCost(c6138.spcost)
+	e2:SetTarget(c6138.sptg)
+	e2:SetOperation(c6138.spop)
+	c:RegisterEffect(e2)
+end
+
+function c6138.aafil(c)
+	return (c:IsSetCard(0x10ed) or c:IsSetCard(0x120e))
+end
+
+function c6138.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	return tc:IsSummonType(SUMMON_TYPE_PENDULUM) and tc:IsControler(tp)
+end
+function c6138.thfil(c)
+	return c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c6138.aafil(c) and (c:GetLevel()==1 or c:GetLevel()==8) and c:IsAbleToHand()
+end
+function c6138.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c6138.thfil,tp,LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
+end
+function c6138.thop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c6138.thfil,tp,LOCATION_EXTRA,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,tp,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+function c6138.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc1=Duel.GetFieldCard(tp,LOCATION_SZONE,6)
+	local tc2=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
+	return tc1 and c6138.aafil(tc1) and tc2 and c6138.aafil(tc2)
+end
+function c6138.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c6138.spfil(c,e,tp)
+	return c6138.aafil(c) and (c:GetLevel()==1 or c:GetLevel()==8) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and (c:IsFaceup() or c:IsLocation(LOCATION_HAND))
+end
+function c6138.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1 and Duel.IsExistingMatchingCard(c6138.spfil,tp,LOCATION_EXTRA+LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function c6138.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c6138.spfil,tp,LOCATION_EXTRA+LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
