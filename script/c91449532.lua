e750873a3cf63aeb5431bbfa74147b7ec5dee4ee
EerieCode <eeriecode@gmail.com>
2016-07-08 22:03:53 +0200

    Added new cards
    "Performapal Allcover Hippo"
    "Performapal Seal Eel"

---
+++ script/c91449532.lua
@@ -0,0 +1,71 @@
+--ＥＭオールカバー・ヒッポ
+--Performapal Allcover Hippo
+--Scripted by Eerie Code
+function c91449532.initial_effect(c)
+	--Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(91449532,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(c91449532.sptg)
+	e1:SetOperation(c91449532.spop)
+	c:RegisterEffect(e1)
+	--Change position
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(91449532,1))
+	e2:SetCategory(CATEGORY_POSITION)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c91449532.postg)
+	e2:SetOperation(c91449532.posop)
+	c:RegisterEffect(e2)
+end
+
+function c91449532.spfil(c,e,tp)
+	return c:IsSetCard(0x9f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c91449532.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c91449532.spfil,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function c91449532.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c91449532.spfil,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		local c=e:GetHandler()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
+		Duel.SpecialSummonComplete()
+	end
+end
+
+function c91449532.posfil(c)
+	local pos=0
+	if POS_DEFENSE then pos=POS_DEFENSE else pos=POS_DEFENCE end
+	return not c:IsPosition(pos)
+end
+function c91449532.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c91449532.posfil,tp,LOCATION_MZONE,0,1,nil) end
+end
+function c91449532.posop(e,tp,eg,ep,ev,re,r,rp)
+	local pos=0
+	if POS_FACEUP_DEFENSE then pos=POS_FACEUP_DEFENSE else pos=POS_FACEUP_DEFENCE end
+	local g=Duel.GetMatchingGroup(c91449532.posfil,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	while tc do
+		Duel.ChangePosition(tc,pos)
+		tc=g:GetNext()
+	end
+end
