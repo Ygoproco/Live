f60a390caeb0febec3468bdbeb136412186abe0c
EerieCode <eeriecode@gmail.com>
2016-07-18 09:19:49 +0200

    Fixed "Abyss Script - Fantasy Magic" (credits to mercury233)

--- script/c7423.lua
+++ script/c7423.lua
@@ -1,6 +1,7 @@
 --魔界台本 「ファンタジー・マジック」
 --Abyss Script - Fantasy Magic
 --Scripted by Eerie Code
+--return to hand effect by mercury233
 function c7423.initial_effect(c)
 	--
 	local e1=Effect.CreateEffect(c)
@@ -40,33 +41,40 @@ function c7423.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetDescription(aux.Stringid(7423,0))
-		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+		local g=Group.CreateGroup()
+		g:KeepAlive()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_DAMAGE_STEP_END)
-		e1:SetCondition(c7423.retcon)
-		e1:SetTarget(c7423.rettg)
+		e1:SetLabelObject(g)
 		e1:SetOperation(c7423.retop)
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		--
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e2:SetDescription(aux.Stringid(7423,0))
+		e2:SetCode(EVENT_BATTLED)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e2:SetLabelObject(e1)
+		e2:SetOperation(c7423.regop)
+		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
 	end
 end
-function c7423.retcon(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToBattle() then return false end
-	local t=nil
-	if ev==0 then t=Duel.GetAttackTarget()
-	else t=Duel.GetAttacker() end
-	e:SetLabelObject(t)
-	return t and t:IsRelateToBattle()
-end
-function c7423.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetLabelObject(),1,0,0)
-end
 function c7423.retop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabelObject():IsRelateToBattle() then
-		Duel.SendtoHand(e:GetLabelObject(),nil,REASON_EFFECT)
+	local g=e:GetLabelObject()
+	if g:GetCount()>0 then
+		local sg=g:Filter(Card.IsRelateToBattle,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		g:Clear()
+	end
+end
+function c7423.regop(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetHandler():GetBattleTarget()
+	if bc and not bc:IsStatus(STATUS_BATTLE_DESTROYED) then
+		local g=e:GetLabelObject():GetLabelObject()
+		g:AddCard(bc)
 	end
 end
 
