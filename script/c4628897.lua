0d36ba9884ea1b8957f450e85a70dbe6cd618abc
Steeldarkeagel <steeldarkeagel@gmail.com>
2016-07-27 12:59:46 +0100

    Imperion Magnum the Superconductive Battlebot
    Fixed effect

--- script/c4628897.lua
+++ script/c4628897.lua
@@ -1,6 +1,5 @@
---Super Conduction Machine Imperion Magnum
+--超電導戦機インペリオン・マグナム
 function c4628897.initial_effect(c)
-
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcCode2(c,75347539,42901635,false,false)
@@ -13,45 +12,47 @@ function c4628897.initial_effect(c)
 	c:RegisterEffect(e1)
 	--negate
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(4628897,0))
 	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_CHAINING)
-	e2:SetCountLimit(1)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCountLimit(1)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(c4628897.discon)
-	e2:SetTarget(c4628897.distg)
-	e2:SetOperation(c4628897.disop)
+	e2:SetCondition(c4628897.negcon)
+	e2:SetTarget(c4628897.negtg)
+	e2:SetOperation(c4628897.negop)
 	c:RegisterEffect(e2)
-	--summon Valk and Vers
+	--special summon
 	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(4628897,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_LEAVE_FIELD)
 	e3:SetCondition(c4628897.spcon)
 	e3:SetTarget(c4628897.sptg)
 	e3:SetOperation(c4628897.spop)
 	c:RegisterEffect(e3)
 end
-
-function c4628897.discon(e,tp,eg,ep,ev,re,r,rp)
-	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev) and rp~=tp and (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE))
+function c4628897.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if ep==tp or c:IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+	return (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
 end
-function c4628897.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c4628897.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
-function c4628897.disop(e,tp,eg,ep,ev,re,r,rp)
+function c4628897.negop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateActivation(ev)
 	if re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
 	end
 end
-
 function c4628897.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:GetReasonPlayer()~=tp and c:IsReason(REASON_EFFECT)
