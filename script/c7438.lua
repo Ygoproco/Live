c9a2558f0f9b79758af481a460d1398bf241ee48
EerieCode <eeriecode@gmail.com>
2016-07-28 21:35:42 +0200

    Added "Darklord Amdusias"

---
+++ script/c7438.lua
@@ -0,0 +1,97 @@
+--堕天使アムドゥシアス
+--Darklord Amdusias
+--Scripted by Eerie Code
+function c7438.initial_effect(c)
+	c:SetSPSummonOnce(7438)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(7438,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,7438)
+	e1:SetCost(c7438.thcost)
+	e1:SetTarget(c7438.thtg)
+	e1:SetOperation(c7438.thop)
+	c:RegisterEffect(e2)
+	--copy card
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(7438,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetCountLimit(1,7438+1)
+	e2:SetCost(c7438.cpcost)
+	e2:SetTarget(c7438.cptg)
+	e2:SetOperation(c7438.cpop)
+	c:RegisterEffect(e2)
+end
+
+function c7438.thcfil(c)
+	return c:IsSetCard(0xef) and c:IsDiscardable()
+end
+function c7438.thfil(c)
+	return c:IsSetCard(0xef) and c:IsAbleToHand()
+end
+function c7438.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsDiscardable() and Duel.IsExistingMatchingCard(c7438.thcfil,tp,LOCATION_HAND,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local g=Duel.SelectMatchingCard(tp,c7438.thcfil,tp,LOCATION_HAND,0,1,1,c)
+	g:AddCard(c)
+	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
+end
+function c7438.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c7438.thfil(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c7438.thfil,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c7438.thfil,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,g:GetCount(),0,0)
+end
+function c7438.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
+
+function c7438.cpcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function c7438.fil(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSetCard(0xef) and c:IsAbleToDeck() and c:CheckActivateEffect(true,true,false)~=nil
+end
+function c7438.cptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then
+		local te=e:GetLabelObject()
+		local tg=te:GetTarget()
+		return tg and tg(e,tp,eg,ep,ev,re,r,rp,0,chkc)
+	end
+	if chk==0 then return Duel.IsExistingTarget(c7438.fil,tp,LOCATION_GRAVE,0,1,nil) end
+	e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,c7438.fil,tp,LOCATION_GRAVE,0,1,1,nil)
+	local te,ceg,cep,cev,cre,cr,crp=g:GetFirst():CheckActivateEffect(false,true,true)
+	Duel.ClearTargetCard()
+	g:GetFirst():CreateEffectRelation(e)
+	local tg=te:GetTarget()
+	e:SetCategory(te:GetCategory())
+	e:SetProperty(te:GetProperty())
+	if tg then tg(e,tp,ceg,cep,cev,cre,cr,crp,1) end
+	te:SetLabelObject(e:GetLabelObject())
+	e:SetLabelObject(te)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+function c7438.cpop(e,tp,eg,ep,ev,re,r,rp)
+	local te=e:GetLabelObject()
+	if not te then return end
+	if not te:GetHandler():IsRelateToEffect(e) then return end
+	e:SetLabelObject(te:GetLabelObject())
+	local op=te:GetOperation()
+	if op then op(e,tp,eg,ep,ev,re,r,rp) end
+	Duel.BreakEffect()
+	Duel.SendtoDeck(te:GetHandler(),nil,2,REASON_EFFECT)
+end
