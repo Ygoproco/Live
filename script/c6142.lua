82084a240e67ad59845689706913e5114801668b
YgoPro-Sahim <loris.cangini@hotmail.it>
2016-08-07 13:29:24 +0200

    fixed "Delta the magnet warrior"

--- script/c6142.lua
+++ script/c6142.lua
@@ -49,7 +49,7 @@ end
 function c6142.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c6142.costfilter,tp,LOCATION_GRAVE,0,3,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c56174248.costfilter,tp,LOCATION_GRAVE,0,3,3,nil)
+	local g=Duel.SelectMatchingCard(tp,c6142.costfilter,tp,LOCATION_GRAVE,0,3,3,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function c6142.spfilter(c,e,tp)
